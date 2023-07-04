ba14a4e7553165657185449fe08e7ceddcdbd51e
Ernest Ampene Junior <116603299+Ernest-Geek@users.noreply.github.com>
2023-07-04 00:33:49 +0000

    Applied Betty Coding style
    Header file needed a space so i created that

--- MAIN.C
+++ MAIN.C
@@ -1,24 +1,26 @@
-#include"election.h"
+#include "election.h"
 
-int main(){
-    while(1){
-        printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
-		char input;
-        scanf(" %c",&input);
-
-        switch(input){
-            case '1':
-                studentPanel();
-                break;
-            case '2':
-                adminPanel();
-                break;
-            case '3':
-                return 0;
-            default:
-                printf("\nInvalid option");
-                getch();
-        }
-    }
-    return 0;
+int main()
+{
+while(1)
+{
+printf("\n\t\t\t   1.Student panel \n\t\t\t   2.Admin panel \n\t\t\t   3.Exit \n\t\t\t   Option:");
+char input;
+scanf(" %c",&input);
+switch(input)
+{
+case '1':
+studentPanel();
+break;
+case '2':
+adminPanel();
+break;
+case '3':
+return (0);
+default:
+printf("\nInvalid option");
+getch();
+}
+}
+return (0);
 }
